f00ff1b27e14045d5f0f55e26c6e459d2d26c1b0
andymartinj <anjd16@student.bth.se>
2017-12-13 21:44:25 +0100

    first commit

---
+++ src/mongodb/mongodb.js
@@ -0,0 +1,59 @@
+/**
+ * A module for database with MongoDB
+ *
+ * @module
+ */
+"use strict";
+
+const mongo = require("mongodb").MongoClient;
+var ObjectId = require('mongodb').ObjectID;
+
+const mongoInit = (dsn, collection) => {
+    const connect = async () => {
+        const db  = await mongo.connect(dsn);
+        const col = await db.collection(collection);
+
+        return col;
+    };
+
+    return {
+        async get(search = {}) {
+            const col = await connect();
+
+            return col.find(search).toArray();
+        },
+        async insert(item) {
+            const col = await connect();
+
+            return col.insert(item);
+        },
+        async update(id, item) {
+            const col = await connect();
+
+            return col.update({_id: ObjectId(id)}, {$set: item });
+        },
+        async delete(id) {
+            const col = await connect();
+
+            return col.remove({_id: ObjectId(id)});
+        },
+        async reset() {
+            const col = await connect();
+
+            await col.deleteMany({});
+            await col.insert({
+                "user": "Server",
+                "message": "Welcome to ChitChatter2000!"
+            });
+
+            return col.find({}).toArray();
+        },
+        async close() {
+            const db  = await mongo.connect(dsn);
+
+            db.close();
+        }
+    };
+};
+
+module.exports = { mongoInit };
---
+++ test/mongodb/mongodb.js
@@ -0,0 +1,66 @@
+"use strict";
+
+const assert = require("assert");
+const mocha = require('mocha');
+const describe = mocha.describe;
+const it = mocha.it;
+const dsn =  process.env.DB_DSN || "mongodb://localhost:27017/chat";
+const db = require("../../src/mongodb/mongodb.js").mongoInit(dsn, 'test');
+
+describe("Test database", function() {
+    describe("Reset collection", function() {
+        it("Should delete everything and insert one message", async () => {
+            await db.reset();
+            const data = await db.get();
+
+            assert.equal(data[0].user, "Server");
+            assert.equal(data[0].message, "Welcome to ChitChatter2000!");
+            await db.close();
+        });
+    });
+    describe("Insert new message", function() {
+        it("Should insert one message", async () => {
+            var item = {
+                user: "Andy",
+                message: "Testing"
+            };
+
+            await db.insert(item);
+            const data = await db.get();
+
+            assert.equal(data[1].user, "Andy");
+            assert.equal(data[1].message, "Testing");
+            await db.close();
+        });
+    });
+    describe("Update message", function() {
+        it("Should update second message in database", async () => {
+            const msg = await db.get();
+            const old = msg[1];
+
+            var item = {
+                user: "Andy",
+                message: "Edited"
+            };
+
+            await db.update(old._id, item);
+            const data = await db.get();
+
+            assert.equal(data[1].user, "Andy");
+            assert.equal(data[1].message, "Edited");
+            await db.close();
+        });
+    });
+    describe("Delete message", function() {
+        it("Should delete second message in database", async () => {
+            const msg = await db.get();
+            const old = msg[1];
+
+            await db.delete(old._id);
+            const data = await db.get();
+
+            assert.equal(data[1], undefined);
+            await db.close();
+        });
+    });
+});
